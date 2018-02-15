e19c7dbd9f2fc4ab260fa7061e20d9381fac41ca
servilla <mark.servilla@gmail.com>
2018-02-15 16:44:03 -0700

    Main Flask app

---
+++ dashboard/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+""":Mod: __init__
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    2/15/18
+"""
+from flask import Flask
+from dashboard.users.views import users
+
+app = Flask(__name__)
+app.register_blueprint(users, url_prefix='/users')
