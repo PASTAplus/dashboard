064d975821b42bbb3767f0ab4aa9fa7b863cd7e9
servilla <mark.servilla@gmail.com>
2018-02-19 21:28:17 -0700

    Add wsgi startup

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+""":Mod: wsgi
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    2/15/18
+"""
+from dashboard import app
+
+if __name__ == "__main__":
+    app.run()
