2dbc2abf5135964e96e7722a5efafe1c034331d1
servilla <mark.servilla@gmail.com>
2018-03-06 16:56:04 -0700

    Add dashboard home views

---
+++ webapp/home/views.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: views.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/6/18
+"""
+
+from flask import Blueprint, render_template
+
+home = Blueprint('home', __name__, template_folder='templates')
+
+@home.route('/')
+@home.route('/index')
+def index():
+    return render_template('index.html')
