3447b628ebc09056cb7f58e20d376600b9984a32
servilla <mark.servilla@gmail.com>
2018-03-06 17:44:54 -0700

    Add logging and Config initialization

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,13 +10,25 @@
 :Created:
     2/15/18
 """
+import logging
+import os
+
+import daiquiri
 from flask import Flask
 
+from webapp.config import Config
 from webapp.auth.views import auth
 from webapp.home.views import home
 from webapp.users.views import users
 
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + '/dashboard.log'
+daiquiri.setup(level=logging.INFO, outputs=(daiquiri.output.File(logfile),))
+logger = daiquiri.getLogger(__name__)
+
 app = Flask(__name__)
+
+app.config.from_object(Config)
 
 app.register_blueprint(auth, url_prefix='/dashboard/auth')
 app.register_blueprint(home, url_prefix='/dashboard')
