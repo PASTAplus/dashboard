ae3b57faa2bb4c31816901e556fc5c6cc903a7d2
servilla <mark.servilla@gmail.com>
2018-03-07 21:39:53 -0700

    Add domain (EDI or LTER) as web form element

--- webapp/auth/forms.py
+++ webapp/auth/forms.py
@@ -13,12 +13,14 @@
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    SelectField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
+    domain_choices = [('edi', 'EDI'), ('lter', 'LTER')]
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in...')
+    domain = SelectField('Domain', choices=domain_choices)
     submit = SubmitField('Sign In')
