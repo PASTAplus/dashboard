f7dbb5764cb4d7b001364c66ae5c606b6dcdf370
servilla <mark.servilla@gmail.com>
2018-03-07 21:40:37 -0700

    Begin support for real login

--- webapp/auth/views.py
+++ webapp/auth/views.py
@@ -14,6 +14,8 @@
 
 from flask import Blueprint, flash, redirect, render_template, url_for
 from webapp.auth.forms import LoginForm
+from webapp import properties
+from webapp.auth.user import User
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 
@@ -22,7 +24,17 @@ auth = Blueprint('auth', __name__, template_folder='templates')
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}, password {}, domain {}'.format(
+            form.username.data, form.password.data, form.domain.data))
+        if form.domain.data == 'edi':
+            dn_suffix = properties.EDI_DN
+        else:
+            dn_suffix = properties.LTER_DN
+        user_id = 'uid=' + form.username.data + ',' + dn_suffix
+        password = form.password.data
+        if user_id not in properties.USERS \
+                or not User.authenticate(user_id=user_id, password=password):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
         return redirect(url_for('home.index'))
-    return render_template('login.html',  title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form)
