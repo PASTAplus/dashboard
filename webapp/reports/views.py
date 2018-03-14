1fed73321ffdc8454a4c4ab97d1d122a66761cc2
Duane Costa <dcosta@lternet.edu>
2018-03-14 10:01:43 -0700

    Add report view to dashboard webapp  - All report rendering is now done using Jinja2 templates

--- webapp/reports/views.py
+++ webapp/reports/views.py
@@ -6,7 +6,7 @@
 :Synopsis:
 
 :Author:
-    servilla
+    Duane Costa
 
 :Created:
     3/6/18
@@ -22,137 +22,42 @@ reports = Blueprint('reports', __name__, template_folder='templates')
 @reports.route('/render_no_public', methods=['GET', 'POST'])
 @login_required
 def render_no_public():
-    report_html = generate_report(report_type='no_public')
-    return render_template('report.html', report_html=report_html)
+    return render_report(report_type='no_public')
 
 
 @reports.route('/render_offline', methods=['GET', 'POST'])
 @login_required
 def render_offline():
-    report_html = generate_report(report_type='offline')
-    return render_template('report.html', report_html=report_html)
-
-
-def generate_report(report_type=None):
-    if report_type is None:
-        no_public_html = render_report(report_type='no_public')
-        offline_html = render_report(report_type='offline')
-        return no_public_html + "\n" + offline_html
-    else:
-        return render_report(report_type=report_type)
+    return render_report(report_type='offline')
 
 
 def render_report(report_type=None):
     if report_type:
         if report_type == 'no_public':
-            return render_no_public()
+            metadata_resources, data_resources = load_no_public()
+            return render_template('report_no_public.html', 
+                                   metadata_resources=metadata_resources, 
+                                   data_resources=data_resources)
         elif report_type == 'offline':
-            return render_offline()
-
-
-def render_no_public():
-    html_strings = []
-
-    with open('./webapp/reports/public_no_access.json') as fh:
+            offline_resources, unparsed_resources = load_offline()
+            return render_template('report_offline.html', 
+                                   offline_resources=offline_resources, 
+                                   unparsed_resources=unparsed_resources)
+            
+           
+def load_no_public():
+    with open('webapp/reports/public_no_access.json') as fh:
         resource_dict = json.load(fh)
         metadata_resources = resource_dict["metadata"]
         data_resources = resource_dict["data"]
-
-        html_strings.append(
-            "<h1>PASTA Resources Lacking Public Read Access</h1>")
-        html_strings.append("<h2>Metadata Resources</h2>")
-        html_strings.append("<table style='border: 1px solid black'>")
-        html_strings.append("    <tr>")
-        html_strings.append("        <th>Package ID</th>")
-        html_strings.append("        <th>Resource ID</th>")
-        html_strings.append("        <th>ACL XML</th>")
-        html_strings.append("    </tr>")
-        for metadata_dict in metadata_resources:
-            html_strings.append("    <tr>")
-            html_strings.append(
-                "        <td>%s</td>" % metadata_dict["package_id"])
-            html_strings.append(
-                "        <td>%s</td>" % metadata_dict["resource_id"])
-            html_strings.append(
-                "        <td>%s</td>" % metadata_dict["acl_xml"])
-            html_strings.append("    </tr>")
-        html_strings.append("</table>")
-
-        html_strings.append("<h2>Data Resources</h2>")
-        html_strings.append("<table style='border: 1px solid black'>")
-        html_strings.append("    <tr>")
-        html_strings.append("        <th>Package ID</th>")
-        html_strings.append("        <th>Resource ID</th>")
-        html_strings.append("        <th>ACL XML</th>")
-        html_strings.append("    </tr>")
-        for data_dict in metadata_resources:
-            html_strings.append("    <tr>")
-            html_strings.append("        <td>%s</td>" % data_dict["package_id"])
-            html_strings.append(
-                "        <td>%s</td>" % data_dict["resource_id"])
-            html_strings.append("        <td>%s</td>" % data_dict["acl_xml"])
-            html_strings.append("    </tr>")
-        html_strings.append("</table>")
-
     fh.close()
-    html_string = "\n".join(html_strings)
-    return html_string
+    return (metadata_resources, data_resources)
 
 
-def render_offline():
-    html_strings = []
-
-    with open('./webapp/reports/offline_data.json') as fh:
+def load_offline():
+    with open('webapp/reports/offline_data.json') as fh:
         resource_dict = json.load(fh)
         offline_resources = resource_dict["offline"]
         unparsed_resources = resource_dict["unparsed"]
-
-        html_strings.append(
-            "<h1>PASTA Data Entities with Offline Distribution</h1>")
-
-        html_strings.append("<h2>Offline Data Resources</h2>")
-        html_strings.append("<table style='border: 1px solid black'>")
-        html_strings.append("    <tr>")
-        html_strings.append("        <th>Package ID</th>")
-        html_strings.append("        <th>Resource ID</th>")
-        html_strings.append("        <th>Object Name</th>")
-        html_strings.append("        <th>Medium Name</th>")
-        html_strings.append("    </tr>")
-        for offline_dict in offline_resources:
-            html_strings.append("    <tr>")
-            html_strings.append(
-                "        <td>%s</td>" % offline_dict["package_id"])
-            html_strings.append(
-                "        <td>%s</td>" % offline_dict["resource_id"])
-            html_strings.append(
-                "        <td>%s</td>" % offline_dict["object_name"])
-            html_strings.append(
-                "        <td>%s</td>" % offline_dict["medium_name"])
-            html_strings.append("    </tr>")
-        html_strings.append("</table>")
-
-        html_strings.append(
-            "<h2>Unparsed Resources (Metadata could not be accessed)</h2>")
-        html_strings.append("<table style='border: 1px solid black'>")
-        html_strings.append("    <tr>")
-        html_strings.append("        <th>Package ID</th>")
-        html_strings.append("        <th>Resource ID</th>")
-        html_strings.append("        <th>Object Name</th>")
-        html_strings.append("        <th>Medium Name</th>")
-        html_strings.append("    </tr>")
-        for unparsed_dict in unparsed_resources:
-            html_strings.append("    <tr>")
-            html_strings.append(
-                "        <td>%s</td>" % unparsed_dict["package_id"])
-            html_strings.append(
-                "        <td>%s</td>" % unparsed_dict["resource_id"])
-            html_strings.append(
-                "        <td>%s</td>" % unparsed_dict["object_name"])
-            html_strings.append(
-                "        <td>%s</td>" % unparsed_dict["medium_name"])
-            html_strings.append("    </tr>")
-        html_strings.append("</table>")
-
     fh.close()
-    html_string = "\n".join(html_strings)
-    return html_string
+    return (offline_resources, unparsed_resources)
