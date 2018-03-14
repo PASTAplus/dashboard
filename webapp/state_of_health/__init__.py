c9c484893ffe0f522731430448113d257e82365c
servilla <mark.servilla@gmail.com>
2018-03-14 11:39:25 -0600

    Add new package state_of_health and status constructors to project

---
+++ webapp/state_of_health/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: __init__.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/14/18
+"""
+
+import logging
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
