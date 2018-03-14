83c7a9c835e3912695c93137ea6f15e7627ec232
servilla <mark.servilla@gmail.com>
2018-03-14 11:40:26 -0600

    Add about.html to home base.html and routes

--- webapp/home/views.py
+++ webapp/home/views.py
@@ -12,12 +12,17 @@
     3/6/18
 """
 from flask import Blueprint, render_template
-from flask_login import login_required
 
 
 home = Blueprint('home', __name__, template_folder='templates')
+
 
 @home.route('/')
 @home.route('/index')
 def index():
     return render_template('index.html')
+
+
+@home.route('/about')
+def about():
+    return render_template('about.html')
