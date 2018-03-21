d5f1877db17ea9d9baba6fee4478659de6a8f595
servilla <mark.servilla@gmail.com>
2018-03-20 21:16:43 -0600

    Make PEP8 compliant

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,18 +11,20 @@
 :Created:
     3/7/18
 """
-
 import logging
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
 import daiquiri
+
+sys.path.insert(0, os.path.abspath('..'))
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 logfile = cwd + '/tests.log'
 daiquiri.setup(level=logging.INFO,
                outputs=(daiquiri.output.File(logfile), 'stdout',))
+
+
 def main():
     return 0
 
