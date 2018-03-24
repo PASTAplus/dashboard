60287e5b756a7720e33c85fc03a261e58790c075
servilla <mark.servilla@gmail.com>
2018-03-24 15:40:33 -0600

    Redirect index requests to health/glance.html

--- webapp/home/views.py
+++ webapp/home/views.py
@@ -11,7 +11,7 @@
 :Created:
     3/6/18
 """
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 from soh.config import Config as soh_Config
 from webapp.health.health_check import SystemState
@@ -25,8 +25,8 @@ home = Blueprint('home', __name__, template_folder='templates')
 def index():
     servers = soh_Config.servers
     system = SystemState()
-    return render_template('index.html', system=system, servers=servers)
-
+    # return render_template('index.html', system=system, servers=servers)
+    return redirect(url_for('health.glance'))
 
 @home.route('/about')
 def about():
