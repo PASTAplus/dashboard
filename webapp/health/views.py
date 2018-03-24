39828ea73e099f03ec80c623631618f049a5a34c
servilla <mark.servilla@gmail.com>
2018-03-24 15:39:54 -0600

    Add health views for health routes

---
+++ webapp/health/views.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: views
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/24/18
+"""
+from flask import Blueprint, render_template
+
+from soh.config import Config as soh_Config
+from webapp.health.health_check import SystemState
+
+health = Blueprint('health', __name__, template_folder='templates')
+
+@health.route('/glance')
+def glance():
+    servers = soh_Config.servers
+    system = SystemState()
+    return render_template('glance.html', system=system, servers=servers)
