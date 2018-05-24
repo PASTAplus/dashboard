af39b8daecff1fac0a18e0ef176b1b89f438dae0
servilla <mark.servilla@gmail.com>
2018-05-23 21:02:14 -0600

    Reorder functions; add get_email

--- webapp/auth/ldap.py
+++ webapp/auth/ldap.py
@@ -20,19 +20,6 @@ from config import Config
 logger = daiquiri.getLogger('ldap: ' + __name__)
 
 
-def is_registered(uid=None):
-    result = False
-    server = Server(Config.LDAP, use_ssl=True, get_info=ALL)
-    filter = Config.LDAP_FILTER.replace(Config.LDAP_UID, uid)
-    try:
-        conn = Connection(server=server, auto_bind=True, receive_timeout=30)
-        result = conn.search(Config.LDAP_BASE, filter)
-        conn.unbind()
-    except Exception as e:
-        logger.error(e)
-    return result
-
-
 def add(uid=None, gn=None, sn=None, email=None, passwd=None):
     result = False
     dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
@@ -57,6 +44,32 @@ def add(uid=None, gn=None, sn=None, email=None, passwd=None):
     return result
 
 
+def bind(uid=None, passwd=None):
+    conn = None
+    dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
+    server = Server(Config.LDAP, use_ssl=True, get_info=ALL)
+    try:
+        conn = Connection(server=server, user=dn, password=passwd,
+                          auto_bind=True, receive_timeout=30)
+    except Exception as e:
+        logger.error(e)
+    return conn
+
+
+def change_password(uid=None, passwd=None, new_passwd=None):
+    result = False
+    dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
+    user_password = {'userPassword': [
+        (MODIFY_REPLACE, [hashed(HASHED_SALTED_SHA, new_passwd)])]}
+    try:
+        conn = bind(uid=uid, passwd=passwd)
+        result = conn.modify(dn, user_password)
+        conn.unbind()
+    except Exception as e:
+        logger.error(e)
+    return result
+
+
 def delete(uid=None):
     result = False
     dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
@@ -72,31 +85,31 @@ def delete(uid=None):
     return result
 
 
-def reset_password(uid=None, new_passwd=None):
-    result = False
-    dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
-    user_password = {'userPassword': [
-        (MODIFY_REPLACE, [hashed(HASHED_SALTED_SHA, new_passwd)])]}
+def get_email(uid=None):
+    email = None
+    filter = Config.LDAP_FILTER.replace(Config.LDAP_UID, uid)
     server = Server(Config.LDAP, use_ssl=True, get_info=ALL)
     try:
         conn = Connection(server=server, user=Config.LDAP_ADMIN,
                           password=Config.LDAP_ADMIN_PASSWORD,
                           auto_bind=True, receive_timeout=30)
-        result = conn.modify(dn, user_password)
+        result = conn.search(Config.LDAP_BASE, filter, attributes=['mail'])
+        if result:
+            entry = conn.entries[0]
+            email = entry['mail']
         conn.unbind()
     except Exception as e:
         logger.error(e)
-    return result
+    return email
 
 
-def change_password(uid=None, passwd=None, new_passwd=None):
+def is_registered(uid=None):
     result = False
-    dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
-    user_password = {'userPassword': [
-        (MODIFY_REPLACE, [hashed(HASHED_SALTED_SHA, new_passwd)])]}
+    filter = Config.LDAP_FILTER.replace(Config.LDAP_UID, uid)
+    server = Server(Config.LDAP, use_ssl=True, get_info=ALL)
     try:
-        conn = bind(uid=uid, passwd=passwd)
-        result = conn.modify(dn, user_password)
+        conn = Connection(server=server, auto_bind=True, receive_timeout=30)
+        result = conn.search(Config.LDAP_BASE, filter)
         conn.unbind()
     except Exception as e:
         logger.error(e)
@@ -120,16 +133,21 @@ def reset_email(uid=None, new_email=None):
     return result
 
 
-def bind(uid=None, passwd=None):
-    conn = None
+def reset_password(uid=None, new_passwd=None):
+    result = False
     dn = Config.LDAP_DN.replace(Config.LDAP_UID, uid)
+    user_password = {'userPassword': [
+        (MODIFY_REPLACE, [hashed(HASHED_SALTED_SHA, new_passwd)])]}
     server = Server(Config.LDAP, use_ssl=True, get_info=ALL)
     try:
-        conn = Connection(server=server, user=dn, password=passwd,
+        conn = Connection(server=server, user=Config.LDAP_ADMIN,
+                          password=Config.LDAP_ADMIN_PASSWORD,
                           auto_bind=True, receive_timeout=30)
+        result = conn.modify(dn, user_password)
+        conn.unbind()
     except Exception as e:
         logger.error(e)
-    return conn
+    return result
 
 
 def main():
