d773eff2ce2112e526da45e66a66bc51f3b4fac2
servilla <mark.servilla@gmail.com>
2019-08-01 16:16:09 -0600

    Add upload report form

--- webapp/reports/forms.py
+++ webapp/reports/forms.py
@@ -12,10 +12,18 @@
     1/6/19
 """
 from flask_wtf import FlaskForm
+from wtforms import DateField
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from wtforms.validators import Optional
 
 
 class PackageIdentifier(FlaskForm):
     package_identifier = StringField('Package Identifier',
                                      validators=[DataRequired()])
+
+
+class UploadReport(FlaskForm):
+    scope = StringField('Package Scope', validators=[DataRequired()])
+    start_date = DateField('Start Date', validators=[Optional()], description='YYYY-MM-DD (defaults to 2013-01-01)')
+    end_date = DateField('End Date', validators=[Optional()], description='YYYY-MM-DD (defaults to today)')
