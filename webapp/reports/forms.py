138278b74ab9096c8a8c94bb4ab3ce8b3d5d9754
Mark Servilla <mark.servilla@gmail.com>
2020-02-16 00:17:24 -0700

    Add site report form

--- webapp/reports/forms.py
+++ webapp/reports/forms.py
@@ -14,9 +14,12 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField
 from wtforms import DateField
+from wtforms import SelectField
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from wtforms.validators import Optional
+
+from webapp.reports.upload_report_stats import get_scopes
 
 
 class PackageIdentifier(FlaskForm):
@@ -24,8 +27,22 @@ class PackageIdentifier(FlaskForm):
                                      validators=[DataRequired()])
 
 
+class SiteReport(FlaskForm):
+    scopes = get_scopes()
+    choices = list()
+    for scope in scopes:
+        choices.append((scope, scope))
+
+    scope = SelectField('Site Scope', choices=choices, default="edi")
+
+
 class UploadReport(FlaskForm):
-    scope = StringField('Package Scope', validators=[DataRequired()])
+    scopes = get_scopes()
+    choices = list()
+    for scope in scopes:
+        choices.append((scope, scope))
+
+    scope = SelectField('Package Scope', choices=choices, default="edi")
     start_date = DateField('Start Date', validators=[Optional()], description='YYYY-MM-DD (defaults to 2013-01-01)')
     end_date = DateField('End Date', validators=[Optional()], description='YYYY-MM-DD (defaults to today)')
     show_title = BooleanField('Show data package title (slower)')
