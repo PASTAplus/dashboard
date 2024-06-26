93d2077a1a240fad6285291991880e753615a898
James Brunt <jwbrunt@gmail.com>
2024-06-26 10:12:46 -0600

    Switch send ip to managed pool

--- webapp/mimemail.py
+++ webapp/mimemail.py
@@ -34,7 +34,7 @@ def send_mail(subject: str, msg: str, to: str) -> bool:
     message["From"] = formataddr((Config.FROM_NAME, Config.FROM))
     message["To"] = formataddr(("", to))
 
-    message.add_header("X-SES-CONFIGURATION-SET", "edi-dedicated")
+    message.add_header("X-SES-CONFIGURATION-SET", "edi-managed")
 
     part = MIMEText(msg, "plain")
     message.attach(part)
